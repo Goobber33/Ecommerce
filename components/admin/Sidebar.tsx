e808307fc1b4c1061f330579e5c51330fbd4999b
Kyle <kcparks1234@gmail.com>
2024-02-16 22:46:09 -0800

    Added links to my sidebar.

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -3,10 +3,19 @@ import React from "react";
 
 export default function Sidebar() {
     return (
-        <div className="bg-slate-800">
-            <Link href="#">
-                Logo
-            </Link>
+        <div className="bg-slate-800 space-y-6">
+            <Link href="#"> Logo </Link>
+            <div className="">
+                <Link href="#"> Dashboard </Link>
+                <Link href="#"> Catalog </Link>
+                <Link href="#"> Customers </Link>
+                <Link href="#"> Markets </Link>
+                <Link href="#"> Farmers </Link>
+                <Link href="#"> Orders </Link>
+                <Link href="#"> Staff </Link>
+                <Link href="#"> Settings </Link>
+                <Link href="#"> Online Store </Link>
+            </div>
         </div>
     )
 }
