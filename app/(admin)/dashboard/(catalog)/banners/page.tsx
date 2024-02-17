84efa05c65039995bbd45d1bfe86d04778823c25
Kyle <kcparks1234@gmail.com>
2024-02-16 17:44:48 -0800

    Added my routes and their starter code

---
+++ app/(admin)/dashboard/(catalog)/banners/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Products() {
+    return (
+        <div>
+            <h2>
+                Banners
+            </h2>
+        </div>
+    );
+}
---
+++ app/(admin)/dashboard/(catalog)/coupons/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Products() {
+    return (
+        <div>
+            <h2>
+                Coupons
+            </h2>
+        </div>
+    );
+}
