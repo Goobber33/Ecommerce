feba6152eec77c2e68767bcdf8878b21f0933385
Kyle <kcparks1234@gmail.com>
2024-03-14 23:14:50 -0700

    Styled the icons and added color to my navbar

--- components/admin/Navbar.tsx
+++ components/admin/Navbar.tsx
@@ -3,26 +3,26 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between bg-gray-700 text-slate-50 h-16 px-8 py-4 fixed top-0 w-full left-60 z-50 pr-80">
+    <div className="flex items-center justify-between bg-gray-700 text-slate-50 h-20 px-8 py-8 fixed top-0 w-full left-60 z-50 pr-80">
       <button>
         <AlignJustify />
       </button>
       <div className="flex space-x-3">
         <button>
-          <Sun />
+          <Sun className="text-green-600" />
         </button>
         <button
           type="button"
           className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white rounded-lg"
         >
-          <Bell />
+          <Bell className="text-green-600" />
           <span className="sr-only">Notifications</span>
-          <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+          <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
             20
           </div>
         </button>
         <button>
-          <User />
+          <User className="text-green-600" />
         </button>
       </div>
     </div>
